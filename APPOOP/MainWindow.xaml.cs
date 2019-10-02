f9914716a89a11a4943d1d1aad38480a923e6edc
PHAMBANHATHUNG <hunggaw3@gmail>
2019-10-02 12:34:54 +0700

    Add project files.

---
+++ APPOOP/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace APPOOP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Student st1 = new Student("Minh");
+            st1.Talk();
+            Student st2 = new Student();
+            st2.StudentName = "Dung";
+            st2.Talk();
+
+            List<Student> studentList = new List<Student>();
+            // Nhập tên sinh viên thứ nhất             
+            studentList.Add(new Student("Minh"));             // Nhập tên sinh viên thứ hai             
+            studentList.Add(new Student("Hoa"));             // Nhập tên sinh viên thứ ba             
+            studentList.Add(new Student("Dung"));  
+            //Hiển thị thông tin các sinh viên trong ListBox             // với thuộc tính Name là lst  
+            foreach (Student st in studentList) { lst.Items.Add(st.StudentName); }
+        }
+
+       
+             
+     }
+}
+
---
+++ APPOOP/Student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace APPOOP
+{
+    class Student
+    {
+        public Student(string stname)
+        {
+            this.name = stname;
+        }
+        public Student()
+        {
+
+        }
+        private string name;
+
+        public string StudentName
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public virtual void Talk()
+        {
+            if (StudentName.Equals("Minh"))
+                MessageBox.Show("My name is" + this.StudentName + "Iam a college student");
+            else if (StudentName.Equals("Dung"))
+                MessageBox.Show("My name is" + this.StudentName + "iam a university student");
+        }
+
+    }
+}
